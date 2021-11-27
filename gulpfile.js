6eb488b649afb856e42498b3830a8c9f73a149d0
Casmir Chukwuma <casmirakachi@gmail.com>
2021-11-27 15:02:30 +0200

    debuging card component

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 const buildStyles = () => {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("uchiha/**/*.scss").pipe(sass()).pipe(dest("css"));
 };
 
 const watchTask = () => {
   //1st arg is an array of file to watch, 2nd arg is a function to run when the files change
-  watch(["*.scss"], buildStyles);
+  watch(["uchiha/**/*.scss"], buildStyles);
 };
 
 exports.default = series(buildStyles, watchTask);
